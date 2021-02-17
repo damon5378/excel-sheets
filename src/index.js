adf0d2d92d52ddb6858b2e403ce95306d4c481f6
damon5378 <iosifov.konstantin@gmail.com>
2021-02-17 19:52:31 +0300

    Finish project config

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./module";
+import "./scss/index.scss";
+
+console.log("====================================");
+console.log("Working....");
+console.log("====================================");
